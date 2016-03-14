3f4b24696f0434ac679f24ca842f89a4a603308a
Dakota Benjamin <dakota.benjamin@gmail.com>
2016-03-14 15:17:13 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,23 @@
 from opendm import log
 from opendm import config
 from opendm import system
+from opendm import context
 
 import sys
 import ecto
+import os
 
 from scripts.odm_app import ODMApp
 
+# Set env variables ##### REMOVE THIS #####
+pypath = (os.path.join(context.superbuild_path, 'install/lib'), 
+          os.path.join(context.superbuild_path, 'install/lib/python2.7/dist-packages'),
+          os.path.join(context.superbuild_path, 'install/lib/python2.7/site-packages'),
+          os.path.join(context.superbuild_path, 'src/opensfm'))
+
+os.environ['LD_LIBRARY_PATH'] = os.path.join(context.superbuild_path, 'install/lib')
+os.environ['PYTHONPATH'] = ":".join(pypath)
+                                    
 
 def usage():
     log.ODM_ERROR('USAGE: %s --project-path [project_path]' % sys.argv[0])
